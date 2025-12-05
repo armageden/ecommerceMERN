22de37b3af7e938e78d2d85b53e5562819ae0d22
arm-td93 <86878487+armageden@users.noreply.github.com>
2025-12-06 04:25:19 +0600

    category image upload

--- server/src/routers/categoryRouter.js
+++ server/src/routers/categoryRouter.js
@@ -8,14 +8,31 @@ const {
     deleteCategory,
 } = require("../controllers/categoryController");
 const { isLoggedIn, isAdmin } = require("../middlewares/auth");
+const { upload } = require("../middlewares/uploadFiles");
+const { validateCategory } = require("../validators/category");
+const { runValidation } = require("../validators");
 
 // Public routes
 categoryRouter.get("/", getCategories);
 categoryRouter.get("/:slug", getCategory);
 
 // Admin only routes
-categoryRouter.post("/", isLoggedIn, isAdmin, createCategory);
-categoryRouter.put("/:slug", isLoggedIn, isAdmin, updateCategory);
+categoryRouter.post(
+    "/",
+    isLoggedIn,
+    isAdmin,
+    upload.single("image"),
+    validateCategory,
+    runValidation,
+    createCategory
+);
+categoryRouter.put(
+    "/:slug",
+    isLoggedIn,
+    isAdmin,
+    upload.single("image"),
+    updateCategory
+);
 categoryRouter.delete("/:slug", isLoggedIn, isAdmin, deleteCategory);
 
 module.exports = categoryRouter;
