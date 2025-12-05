72fcc74933f79fd5a1cea993571920d969de3453
arm-td93 <86878487+armageden@users.noreply.github.com>
2025-12-05 23:33:38 +0600

    add category model

---
+++ server/src/models/categoryModel.js
@@ -0,0 +1,24 @@
+const { Schema, model } = require("mongoose");
+
+const categorySchema = new Schema(
+    {
+        name: {
+            type: String,
+            required: [true, "Category name is required"],
+            trim: true,
+            unique: true,
+            minlength: [3, "Category name must be at least 3 characters"],
+            maxlength: [31, "Category name cannot exceed 31 characters"],
+        },
+        slug: {
+            type: String,
+            required: [true, "Category slug is required"],
+            lowercase: true,
+            unique: true,
+        },
+    },
+    { timestamps: true }
+);
+
+const Category = model("Category", categorySchema);
+module.exports = Category;
